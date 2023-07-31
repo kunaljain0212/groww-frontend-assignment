0fd6c1e7ab039f2353a5397e2a17cb8cc78c72f4
Kunal Jain <jainkunal209@gmail.com>
2023-07-31 17:32:27 +0530

    feat: custom image wrapper with dyanmic image blur hash

---
+++ app/components/blur-hash-image-wrapper/BlurHashImageLoader.js
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+import { customLoader } from "@/app/utils/custom-image-loader";
+
+export default function BlurHashImageWrapper({ orgUrl, width, height, alt }) {
+  return (
+    <Image
+      loader={customLoader}
+      style={{
+        width: "100%",
+        height: "auto",
+        borderRadius: "8px",
+      }}
+      src={orgUrl}
+      alt={alt}
+      blurDataURL={`/_next/image?url=${encodeURIComponent(orgUrl)}&q=1&w=128`}
+      placeholder="blur"
+      width={width}
+      height={height}
+      quality={60}
+    />
+  );
+}
